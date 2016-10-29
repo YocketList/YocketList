5cb0bcffb06550c1528db1f4ecdf8bc9dfc3a22e
Martin-Ting <martinhting@gmail.com>
2016-10-29 03:24:46 -0700

    chore: adds descriptive comments to playerapp, layout, queue

--- client/layout.js
+++ client/layout.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router';
 
-
+/**
+ * Layout is the react-router component that defines the structure of the page.
+ * this.props.children changes based on the current route.
+ */
 class Layout extends React.Component {
  render() {
     return (
--- client/playerapp.js
+++ client/playerapp.js
@@ -12,9 +12,17 @@ class PlayerApp extends React.Component {
       urls : []
     };
     this.handlePlayerEnd = this.handlePlayerEnd.bind(this);
+
+    //initiate socket connection and set up listeners
     this.socket = io.connect('http://localhost:3000');
     this.initSocket();
   }
+  /**
+   * This is where the listers for this.socket go.
+   * on[newData] -> implies there is a change in data on the backend
+   *                the callback will make a GET request and update state
+   *                with the new list of Youtube URLs
+   */
   initSocket () {
     this.socket.on('newdata', (data) => {
       console.log("got new data");
@@ -23,23 +31,32 @@ class PlayerApp extends React.Component {
       });
     });
   }
+  /**
+   * We grab our initial set of data here after the first render
+   * has been made.
+   */
   componentDidMount(){
     $.get("http://localhost:3000/queue").done((data) => {
       this.setState({urls: data});
     });
-
   }
+  /**
+   * handleStateChange is an event listener for the react-youtube
+   * componenet's state. The states are as follows:
+   * UNSTARTED: -1, ENDED: 0, PLAYING: 1, PAUSED: 2, BUFFERING: 3, CUED: 5
+   */
   handleStateChange(event){
+    // CUED was a good option for enabling "auto play" because it waits
+    // until the player is loaded (-1) and then the video is cued ready to play
     if(event.data === 5){
       event.target.playVideo();
     }
   }
-
+/**
+ * This method makes a post request to the server with the body {method: 'delete'}
+ * This removes an item from the db and notifies all clients with the newdata event.
+ */
   handlePlayerEnd(event){
-    // swap out video for Youtube Player
-    // Make request to server to notify all clients that the video ended
-    //  so they can remove the video from the queue 
-    //  another get request or just remove top item
     $.ajax({
       type: "POST",
       url: "http://localhost:3000/queue",
@@ -53,7 +70,7 @@ class PlayerApp extends React.Component {
     // Can we make it so the current video played is not 
     // displayed in the queue?
     if(this.state.urls.length > 1)
-    var videoUrl = this.state.urls[0].split('=')[1];
+      var videoUrl = this.state.urls[0].split('=')[1];
     return (
     <div className="youtube-wrapper">
       <Youtube videoId={videoUrl} onEnd={this.handlePlayerEnd} onStateChange={this.handleStateChange}/>
