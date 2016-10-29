3c9dbffdbb77f98be5c89b3d74bb99addb236492
Martin-Ting <martinhting@gmail.com>
2016-10-28 20:26:14 -0700

    feat: updated server logic

--- server/server.js
+++ server/server.js
@@ -2,27 +2,48 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyparser = require('body-parser');
+
+/* Database */
+const qArray = ['https://www.youtube.com/watch?v=H8H5tNVE_sY', 'https://www.youtube.com/watch?v=CrF3J6ukER8', 'https://www.youtube.com/watch?v=_9vK_F0XnfA'];
+
+/* Express Middleware */
 app.use(bodyparser.json());
-const qArray = [];
 
 app.get('/', (req, res) => {
-  res.status(200).send("Yocket List!");
+  res.status(200).send("Yocket List! Where Yockets meets Lists. Yocket List!");
 });
 
-app.post('/qeue', (req, res) => {
-  console.log(req.body);
-  io.emit('newqeue', req.body);
-  res.end();
-})
-app.get('/qeue', (req, res) => {
-  // fetch data from db
-  io.emit('newqeue', {message: 'made a get request to /qeue'});
-  res.send(['1','2']);
-})
+// Post body do /queue should be formatted like so:
+// req.body { link: '<new Youtube link>'}
+app.get('/queue', (req, res) => {
+  console.log(`/queue :: [GET] sending data ${qArray}`);
+  // io.emit();
+  res.status(200).send(qArray);
+});
 
+app.post('/queue', (req, res) => {
+  console.log(`/queue :: [POST] got data ${req.body.link}`);
+  if(!req.body.link){
+    res.status(404).send("no data supplied");
+    return;
+  }
+  qArray.push(req.body.link);
+  io.emit('newdata', qArray.length);
+  res.status(200).send("");
+  res.end();
+});
+
+app.delete('/queue', (req, res) => {
+  console.log(`/queue :: [DELETE] removing first item from ${qArray}`);
+  qArray.shift();
+  console.log(`/queue :: [DELETE] result of delete ${qArray}`);
+  io.emit('newdata', qArray.length);
+  res.status(200).send("");
+});
+
+/* Socket and Server Setup */
 io.on('connect', (socket) => {
-  console.log('user connected')
-  io.emit('hello', {suu: 'dude'});
+  console.log(`User connected ${socket.id}`)
 })
 
 http.listen(3000, () => {
